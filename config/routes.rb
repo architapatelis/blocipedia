9353f760613684de452c815a0dc41c55b0749396
Archita Patel <architapatelis@gmail.com>
2017-04-30 21:32:16 -0700

    create wiki controller and customize views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :wikis, only: [:index, :show, :new, :edit, :create, :update, :destroy]
   devise_for :users
   get 'about' => 'welcome#about'
   # default page the app loads when we navigate to the home page URL.
